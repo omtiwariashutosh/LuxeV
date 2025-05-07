b29990ce14cfccf8baa065668d4038d57744c47c
Ashutosh <ashutoshkumartiwari888@gmail.com>
2025-05-07 20:18:26 +0530

    Initial commit - elegent product store

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,13 @@
-const openMenu = document.getElementById("openMenu");
-const closeMenu = document.getElementById("closeMenu");
-const fullscreenNav = document.getElementById("fullscreenNav");
+document.addEventListener("DOMContentLoaded", () => {
+  const openMenu = document.getElementById("openMenu");
+  const closeMenu = document.getElementById("closeMenu");
+  const fullscreenNav = document.getElementById("fullscreenNav");
 
-openMenu.addEventListener("click", () => {
-  fullscreenNav.classList.add("active");
-});
-
-closeMenu.addEventListener("click", () => {
-  fullscreenNav.classList.remove("active");
-});
-// ****************************
-const navLinks = document.querySelectorAll(".nav-links a");
-const imageBg = document.querySelector(".nav-image-bg");
-
-navLinks.forEach((link) => {
-  link.addEventListener("mouseenter", () => {
-    imageBg.style.transform = "scale(1.1)";
+  openMenu.addEventListener("click", () => {
+    fullscreenNav.classList.add("active");
   });
 
-  link.addEventListener("mouseleave", () => {
-    imageBg.style.transform = "scale(1)";
+  closeMenu.addEventListener("click", () => {
+    fullscreenNav.classList.remove("active");
   });
 });
